53c682125c187f6104094b7b6563b550cc7b1ee2
Oguzz <Oguzz@OGUZHAN_PC>
2023-02-18 22:02:22 +0300

    Add project files.

---
+++ RabbitMQ_Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+// Bağlantı Oluşturma.
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://ajgenuck:uxjM3ZNS4MoDEmnbXOyPtlfixMZZBA4A@shark.rmq.cloudamqp.com/ajgenuck");
+
+// Bağlantıyı aktifleştirme ve kanal açmak.
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma.
+channel.QueueDeclare(queue:"example-queue",exclusive:false);
+
+//Queue mesaj gönderme
+// RabbitMQ kuyruğa ataçağı mesajları byte türünde kabul eder. Bizimde mesajları byte'a dönüştrümemiz gerek.
+byte[] message = Encoding.UTF8.GetBytes("Selamunaleyküm");
+channel.BasicPublish(exchange:"",routingKey: "example-queue",body:message);
+
+Console.ReadLine();
